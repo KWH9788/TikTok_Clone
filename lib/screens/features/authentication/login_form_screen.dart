60aab3633609f97b7f9eb2af2656284f7fa41202
KWH9788 <dngus9788@gmail.com>
2023-03-27 16:36:41 +0900

    feat: Update login Screen

---
+++ lib/screens/features/authentication/login_form_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/authentication/widgets/form_button.dart';
+
+class LoginFormScreen extends StatefulWidget {
+  const LoginFormScreen({super.key});
+
+  @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
+  void _onSubmitTap() {
+    if (_formKey.currentState != null) {
+      // 유효성 검사
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        print(_formData.values);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size32,
+          vertical: Sizes.size24,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _formData['Email'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _formData['Password'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: FormButton(disabled: false),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
