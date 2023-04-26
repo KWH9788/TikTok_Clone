3a7cd4e2382e26bd41acd6cef4f53699c4a341eb
KWH9788 <dngus9788@gmail.com>
2023-04-26 17:32:34 +0900

    fix: Video issues when changing tabs

--- lib/screens/features/videos/widgets/video_post_widget.dart
+++ lib/screens/features/videos/widgets/video_post_widget.dart
@@ -55,6 +55,9 @@ class _VideoPostState extends State<VideoPost>
         !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
     }
+    if (_videoPlayerController.value.isPlaying && info.visibleFraction == 0) {
+      onTap();
+    }
   }
 
   void onTap() {
