4e5ed75a0f56914b82784e9c42a6ea34b3fe28ad
KWH9788 <dngus9788@gmail.com>
2023-05-18 17:31:56 +0900

    feat: Update profile ui

--- lib/screens/features/user/user_profile_screen.dart
+++ lib/screens/features/user/user_profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -11,123 +13,178 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          stretch: true,
-          floating: true,
-          backgroundColor: Colors.teal,
-          collapsedHeight: 80,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-              StretchMode.fadeTitle,
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("KWH9788"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.gear,
+                  size: Sizes.size20,
+                ),
+              ),
             ],
-            background: Image.asset(
-              "assets/images/space-gab40051d8_1920.jpg",
-              fit: BoxFit.cover,
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 45,
+                  foregroundColor: Colors.blue,
+                  foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/88845841',
+                  ),
+                  child: Text("우현"),
+                ),
+                Gaps.v14,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "@kwh9788",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      color: Colors.blue.shade300,
+                      size: Sizes.size14,
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                SizedBox(
+                  height: Sizes.size48,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      makeIndicator("37", "Following"),
+                      VerticalDivider(
+                        color: Colors.grey.shade300,
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      makeIndicator("10.5M", "Followers"),
+                      VerticalDivider(
+                        color: Colors.grey.shade300,
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      makeIndicator("149.3M", "Likes"),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                FractionallySizedBox(
+                  widthFactor: 0.4,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Sizes.size14,
+                    ),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(Sizes.size2),
+                    ),
+                    child: const Text(
+                      "Follow",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                Gaps.v12,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: Sizes.size28),
+                  child: Text(
+                    "All highlights and where to watch live matches on FIFA+",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Gaps.v24,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size12,
+                    ),
+                    Gaps.h4,
+                    Text(
+                      "https://www.fifa.com/fifaplus/en/home",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+                Gaps.v16,
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(
+                        color: Colors.grey.shade300,
+                        width: 0.5,
+                      ),
+                    ),
+                  ),
+                  child: const TabBar(
+                    labelColor: Colors.black,
+                    indicatorColor: Colors.black,
+                    labelPadding: EdgeInsets.symmetric(vertical: Sizes.size12),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: Icon(Icons.grid_4x4_rounded),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: FaIcon(FontAwesomeIcons.heart),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            title: const Text("Hello!"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column makeIndicator(String num, String category) {
+    return Column(
+      children: [
+        Text(
+          num,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: Sizes.size20,
           ),
         ),
-        SliverPersistentHeader(
-          delegate: CustomDelegate(),
-          pinned: true,
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              alignment: Alignment.center,
-              color: Colors.amber[100 * (index % 9)],
-              child: Text(
-                "item $index",
-              ),
-            ),
+        Gaps.v5,
+        Text(
+          category,
+          style: TextStyle(
+            color: Colors.grey.shade500,
+            fontSize: Sizes.size14,
           ),
-          itemExtent: 100,
         ),
-        SliverPersistentHeader(
-          delegate: CustomDelegateGrid(),
-          pinned: true,
-        ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              alignment: Alignment.center,
-              color: Colors.blue[100 * (index % 9)],
-              child: Text(
-                "item $index",
-              ),
-            ),
-          ),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1,
-          ),
-        )
       ],
     );
-  }
-}
-
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.amber,
-      child: const Text(
-        "List",
-        style: TextStyle(
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-  @override
-  double get maxExtent => 100;
-
-  @override
-  double get minExtent => 80;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
-  }
-}
-
-class CustomDelegateGrid extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.blue,
-      child: const Text(
-        "Grid",
-        style: TextStyle(
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-  @override
-  double get maxExtent => 100;
-
-  @override
-  double get minExtent => 80;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
   }
 }
